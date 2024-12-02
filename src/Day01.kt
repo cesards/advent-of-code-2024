1f528d40fa3ce8196c2ab32057c68a01647d612f
Cesar Diez Sanchez <cesaryomismo@gmail.com>
2024-12-02 09:18:16 -0500

    Day 1 solution (first iteration).

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,39 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+import kotlin.math.abs
+
+fun day01() {
+    val lines = readInput("Day01-Input")
+
+    // Part 1
+    val first =  mutableListOf<Int>()
+    val second =  mutableListOf<Int>()
+    lines.forEach { line ->
+        val coordinates = line.split(" ").filterNot(String::isEmpty).map { it.toInt() }
+        first.add(coordinates[0])
+        second.add(coordinates[1])
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    part1(first, second).println()
+    part2(first, second).println()
 }
+
+private fun part1(first: MutableList<Int>, second: MutableList<Int>) : Int {
+    first.sort()
+    second.sort()
+
+    val min = first.size.coerceAtMost(second.size)
+
+    var sum = 0
+    for (i in 0..<min) {
+        sum += abs(first[i] - second[i])
+    }
+
+    return sum
+}
+
+
+fun part2(first: MutableList<Int>, second: MutableList<Int>) : Int {
+    return first.sumOf { left ->
+        left * second.count { left == it }
+    }
+}
+
