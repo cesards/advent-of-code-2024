6bd27e2c3330952e6f43504d45a072ff426f6340
Cesar Diez Sanchez <cesaryomismo@gmail.com>
2024-12-03 23:27:28 -0500

    Day 3 solution part 2 (first iteration).

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,6 +1,12 @@
+import java.util.regex.Pattern
 
 fun main() {
     val lines = readInput("Day03-Input")
+    part01(lines)
+    part02(lines)
+}
+
+private fun part01(lines: List<String>) {
     val regex = Regex("mul\\(\\d{1,3},\\d{1,3}\\)")
     var sum = 0L
     lines.map { line ->
@@ -9,6 +15,26 @@ fun main() {
                     it.value.substringAfter(",").substringBefore(")").toInt()
             sum += aha
         }
+    }
+    println(sum)
+}
+
+private fun part02(lines: List<String>) {
+    val donts = Regex("don't\\(\\)(.*?)do\\(\\)")
+    val regex = Regex("mul\\(\\d{1,3},\\d{1,3}\\)")
+
+    // We need to put lines together if we want to make this work.
+    val word = StringBuilder()
+    lines.forEach { line -> word.append(line) }
+
+    val pattern = Pattern.compile(donts.pattern)
+    val processedLine = pattern.matcher(word.toString()).replaceAll("")
+
+    var sum = 0L
+    regex.findAll(processedLine).forEach {
+        val aha = it.value.substringBefore(",").substringAfter("(").toInt() *
+                it.value.substringAfter(",").substringBefore(")").toInt()
+        sum += aha
     }
     println(sum)
 }
